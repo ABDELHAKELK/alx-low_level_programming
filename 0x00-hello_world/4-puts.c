396b5005af0bbc4b0edc94cbdc5331fc1dae55fb
abdelhak <abdelhak.elkeddaoui1@gmail.com>
2023-09-07 18:52:38 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+/*
+ *
+ *hello guys
+*/
+       	
+puts(" /"Programming is like building a multilingual puzzle ");
+
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+/* hello
+ *
+ * */
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+
+
+return 0;
+}
